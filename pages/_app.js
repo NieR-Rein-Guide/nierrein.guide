5ccdfcfa02b5e2f659b14085417f811a422964c8
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-08-18 22:35:31 +0200

    fix font loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
+/*  */
 import "tailwindcss/tailwind.css";
 import "../src/index.scss";
+
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/ui/material/material322002_standard.png" />
+        {/* eslint-disable-next-line */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant:wght@300;400;500&amp;display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   );
