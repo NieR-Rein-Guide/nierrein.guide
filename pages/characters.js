100f0a5ce86164f0578faf51f2f0a679dd798457
Clement Benard <contact@clementbenard.com>
2021-08-19 13:50:37 +0200

    Added basic character info

---
+++ pages/characters.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Head from "next/head";
+
+import Layout from "../src/Layout";
+import GuerillaTimers from "../components/GuerillaTimers";
+import Socials from "../components/Socials";
+import CHARACTERS from "../models/character";
+import CharacterInfo from "../components/CharacterInfo";
+
+export default function Home() {
+  const [renderClientSideComponents, setRenderClientSideComponents] =
+    useState(false);
+
+  useEffect(() => setRenderClientSideComponents(true), []);
+
+  return (
+    <Layout>
+      <Head>
+        <title>NieR Re[in] Global Guide & Database</title>
+      </Head>
+
+      {CHARACTERS.map((char, index) =>
+        <CharacterInfo key={index} character={char}/>
+      )}
+    </Layout>
+  );
+}
