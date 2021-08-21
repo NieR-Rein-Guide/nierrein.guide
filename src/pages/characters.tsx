d75771c6555d6e5b133b4bd1c9e29dd245dc86fd
Clement Benard <contact@clementbenard.com>
2021-08-21 17:58:19 +0200

    Most UI done

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -1,79 +1,114 @@
 import Head from "next/head";
 
 import Layout from "@components/Layout";
-import CHARACTERS from "@models/character";
-import { allCostumes, CostumeInfo, typedCharacters, typedCostumes } from "@models/character";
-import CharacterInfo from "@components/CharacterInfo";
-import { allCharacters, costumeIDToIconURL, characterToIconURL } from "libs/nier_icons";
-import { useState } from "react";
+import { CostumeInfo, typedCharacters } from "@models/character";
+import CostumeDetails from "@components/CharacterInfo";
+import { Dispatch, SetStateAction, useState } from "react";
 
 
+function CharacterRows({
+  currentCostume,
+  setCostume,
+}: {
+  currentCostume: CostumeInfo,
+  setCostume: Dispatch<SetStateAction<CostumeInfo>>,
+}) {
+  const characters = Array.from(typedCharacters.values()).map(chars => chars[0])
+
+  const firstRow: CostumeInfo[] = []
+  const secondRow: CostumeInfo[] = []
+  characters.forEach((costume, index) => {
+    if (index%2 == 1) {
+      firstRow.push(costume)
+    } else {
+      secondRow.push(costume)
+    }
+  })
+  
+  return (
+    <div className="relative self-center h-32 mt-20 mb-20">
+      <div className="flex">
+        {firstRow.map((costume) =>
+          <div className="relative" onClick={() => {
+            setCostume(costume)
+          }}>
+            <div className={`absolute text-sm bottom-20 right-0 flex justify-center text-white ${costume.character == currentCostume.character ? '' : 'text-opacity-60'}`} style={{ width: '56px' }}>
+              {costume.character}
+            </div>
+            <div className={`iso-bg ${costume.character == currentCostume.character ? 'active' : ''}`}>
+              <img style={{
+                minWidth: '74px',
+                maxWidth: '74px',
+                minHeight: '74px',
+                maxHeight: '74px',
+              }} title={costume.character} src={costume.iconURL} key={costume.character} />
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="absolute flex" style={{
+        left: '-40px',
+        top: '42px',
+      }}>
+        {secondRow.map((costume) =>
+          <div className="relative" onClick={() => {
+            setCostume(costume)
+          }}>
+            <div className={`absolute text-sm top-20 right-0 flex justify-center text-white ${costume.character == currentCostume.character ? '' : 'text-opacity-60'}`} style={{ width: '56px' }}>
+              {costume.character}
+            </div>
+            <div className={`iso-bg ${costume.character == currentCostume.character ? 'active' : ''}`} >
+              <img style={{
+                minWidth: '74px',
+                maxWidth: '74px',
+                minHeight: '74px',
+                maxHeight: '74px',
+              }} title={costume.character} src={costume.iconURL} key={costume.character} />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+function CharacterCostumes({
+  currentCostume,
+  setCostume,
+}: {
+  currentCostume: CostumeInfo,
+  setCostume: Dispatch<SetStateAction<CostumeInfo>>,
+}) {
+  return (
+    <div className="flex flex-wrap justify-center gap-2 my-2">
+      {typedCharacters.get(currentCostume.character).map(costume => (
+        <div key={costume.id}
+          className={`flex items-center border-beige border-2 p-2 ${currentCostume.id == costume.id ? '' : 'border-opacity-30'}`}
+          onClick={() => setCostume(costume)}>
+          {costume.name.en}
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export default function Home() {
-  const characters = allCharacters()
-  const mid = characters.length / 2
-  const firstRow = characters.slice(0, mid)
-  const secondRow = characters.slice(mid)
-  const [currentCostume, setCostume] = useState(typedCostumes[0])
+
+
+  const [currentCostume, setCostume] = useState(
+    typedCharacters.values().next().value[0] as CostumeInfo
+  )
+
   return (
     <Layout>
       <Head>
         <title>NieR Re[in] Global Guide & Database</title>
       </Head>
 
-      <div>
-        {Array.from(typedCharacters.entries()).map(([character, costumes]) => (
-          <div key={character} className="flex items-center gap-2">
-            <img src={costumeIDToIconURL(costumes[0].id)} alt="" />
-            {character}:
-            {costumes.map(costume => (
-              <div key={costume.id} className="flex items-center border" onClick={() => setCostume(costume)}>
-                {costume.name.en}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      {/* <div className="grid grid-cols-4">
-        {typedCostumes.map(costume => (
-          <div key={costume.id} className="flex items-center" onClick={()=>setCostume(costume)}>
-            <img src={costumeIDToIconURL(costume.id)} alt=""/>
-            {costume.character} ({costume.name.en})
-          </div>
-        ))}
-      </div> */}
+      <CharacterRows {...{ setCostume, currentCostume }} />
+      <CharacterCostumes {...{ setCostume, currentCostume }} />
 
-      <CharacterInfo character={currentCostume}></CharacterInfo>
-      {/* <div className="relative self-center h-32">
-        <div className="flex">
-          {firstRow.map((char) =>
-            <div className="iso-bg" >
-              <img style={{
-              minWidth: '74px',
-              maxWidth: '74px',
-              minHeight: '74px',
-              maxHeight: '74px',
-            }} title={char} src={characterToIconURL(char)} key={char}/>
-            </div>
-          )}
-        </div>
-        <div className="absolute flex" style={{
-          left: '-40px',
-          top: '40px',
-        }}>
-          {secondRow.map((char) =>
-            <div className="iso-bg" >
-              <img style={{
-              minWidth: '74px',
-              maxWidth: '74px',
-              minHeight: '74px',
-              maxHeight: '74px',
-            }} title={char} src={characterToIconURL(char)} key={char}/>
-            </div>
-          )}
-        </div>
-      </div> */}
-
+      <CostumeDetails costume={currentCostume}></CostumeDetails>
     </Layout>
   );
 }
