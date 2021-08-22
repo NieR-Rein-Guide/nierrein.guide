5b6f775994e1dfd75d755670e2e77851a4385d43
Efren Aguilar <efrenaguilar95@hotmail.com>
2021-08-21 20:55:51 -0700

    Add card component
    Create separate card component from what I made yesterday

---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+export default function Card()
+{
+    return(
+        <div className="card-container">
+            <div className="card">
+                <img src="/character/ch001001_full.png"></img>
+            </div>
+        </div>
+    );
+}
--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -10,30 +10,8 @@ export default function CardGrid()
         //     <div></div>
         // </div>
 
-        <div className="card-container">
-            <div className="card">
-                <div className="card-holder-temp">
-                    <img src="/character/ch001001_full.png"></img>
-                </div>
-            </div>
-            <br></br>
-            <div className="card">
-                <div className="card-holder-temp">
-                    <img src="/character/ch001002_full.png"></img>
-                </div>
-            </div>
-            <br></br>
-            <div className="card">
-                <div className="card-holder-temp">
-                    <img src="/character/ch001003_full.png"></img>
-                </div>
-            </div>
-            <br></br>
-            <div className="card">
-                <div className="card-holder-temp">
-                    <img src="/character/ch009001_full.png"></img>
-                </div>
-            </div>
+        <div>
+            
         </div>
     );
 }
