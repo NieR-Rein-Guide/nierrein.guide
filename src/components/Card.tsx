f67bc5b6a91dee324efe60f5882151ea2f2a4037
Efren Aguilar <efrenaguilar95@hotmail.com>
2021-08-21 22:25:22 -0700

    Added aspect ratio css
    So apparently this is a thing, pain.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,8 @@
 export default function Card()
 {
     return(
-        <div className="card-container">
-            <div className="card">
-                <img src="/character/ch001001_full.png"></img>
-            </div>
+        <div className="card">
+            <img src="/character/ch001001_full.png"></img>
         </div>
     );
 }
