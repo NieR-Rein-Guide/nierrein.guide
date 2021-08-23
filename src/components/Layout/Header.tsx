b4293284e1318aa9aba7b7224e60beaa8d3d211e
Nemesis <keziah.moselle@gmail.com>
2021-08-23 15:44:45 +0200

    feat: add animated logo

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,20 +1,58 @@
 import classNames from "classnames";
-import { useRouter } from "next/dist/client/router";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
-import logoImage from "../../../public/logo-256.png";
 import { NAVIGATION } from "config/constants";
+import Lottie from "react-lottie-player";
+import logoData from "../../lottie/logo.json";
+import { useEffect, useState } from "react";
 
 export default function Header(): JSX.Element {
   const router = useRouter();
+  const defaultAnimationPosition = 80;
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [animationPosition, setAnimationPosition] = useState(
+    defaultAnimationPosition
+  );
+
+  function start() {
+    setIsAnimating(true);
+  }
+
+  function done() {
+    setIsAnimating(false);
+  }
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", start);
+    router.events.on("routeChangeComplete", done);
+    router.events.on("routeChangeError", done);
+
+    return () => {
+      router.events.off("routeChangeStart", start);
+      router.events.off("routeChangeComplete", done);
+      router.events.off("routeChangeError", done);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <header className="container">
       <div className="flex flex-col justify-center items-center flex-wrap gap-y-8 px-8 py-6">
         <Link href="/" passHref={true}>
           <a className="flex items-center relative group">
-            <span className="h-20 w-20 mr-4 z-0 absolute -top-16 left-1/2 transform -translate-x-1/2 scale-100 group-hover:scale-110 transition-transform ease-out-cubic">
-              <Image className="" src={logoImage} alt="Logo" />
+            <span
+              onMouseEnter={start}
+              onMouseLeave={done}
+              className="h-20 w-20 mr-4 z-0 absolute -top-16 left-1/2 transform -translate-x-1/2 scale-100 group-hover:scale-110 transition-transform ease-out-cubic"
+            >
+              <Lottie
+                animationData={logoData}
+                play={isAnimating}
+                goTo={animationPosition}
+                speed={0.7}
+                style={{ width: 84, height: 84 }}
+              />
             </span>
             <h1 className="text-3xl text-center z-10 drop-shadow-xl mt-4">
               NieR Re[in] Guide
