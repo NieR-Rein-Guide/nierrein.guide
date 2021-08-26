6f8850d85ec876453141e419b86832db970b9ad0
Nemesis <keziah.moselle@gmail.com>
2021-08-26 09:01:09 +0200

    feat: add ackee

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,12 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/favicon.png" />
           <link rel="apple-touch-icon" sizes="64x64" href="/favicon.png" />
           <link rel="icon" type="image/png" sizes="64x64" href="/favicon.png" />
+          <script
+            async
+            src="https://analytics.nierrein.guide/tracker.js"
+            data-ackee-server="https://analytics.nierrein.guide"
+            data-ackee-domain-id="740ae9bc-5bd5-426f-aa18-4f9ae79b0671"
+          ></script>
         </Head>
         <body>
           <Main />
