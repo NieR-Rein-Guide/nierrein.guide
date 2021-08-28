d0a9cdbe8a7d32efb354986a4d95b862929a35a4
Efren Aguilar <efrenaguilar95@hotmail.com>
2021-08-27 19:02:49 -0700

    Add changes from keziahs review

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,13 +14,13 @@ function CharacterCard({ costume }: { costume: CostumeInfo }): JSX.Element {
   return (
     <Link href={"/characters/" + costume.character + "/" + costume.name.en.replace(" ", "-")}>
       <a>
-        <div className={"card " + "rarity" + costume.stars}>
+        <div className={`card rarity${costume.stars}`}>
           <div className="card-art">
             <div className="element">
               <img src="\icons\elements\dark.png"></img>
             </div>
             <div className="weapon">
-              <img src="\icons\weapons\gun.png"></img>
+              <img src="/icons/weapons/gun.png"></img>
             </div>
             <Image
               src={costume.illustrationURL}
