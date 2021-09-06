9da755a09a317e8f3223b9d9540bab050c5d9e44
Nemesis <keziah.moselle@gmail.com>
2021-09-06 17:26:58 +0200

    feat: add start, end and event period dates on a single event page

--- src/pages/event/[slug].tsx
+++ src/pages/event/[slug].tsx
@@ -8,6 +8,8 @@ import marked from "marked";
 import { useRouter } from "next/router";
 import { Event } from "@models/types";
 import { getAllEvents, getEvent } from "@models/event";
+import Lines from "@components/decorations/Lines";
+import { differenceInDays, formatDistanceToNow } from "date-fns";
 
 interface eventProps {
   event: Event;
@@ -69,6 +71,42 @@ export default function SingleEvent({ event }: eventProps): JSX.Element {
             {event?.poll?.embed && (
               <div dangerouslySetInnerHTML={{ __html: event.poll.embed }}></div>
             )}
+          </div>
+
+          <div className="my-8">
+            <Lines containerClass="flex-col items-center justify-center">
+              {Date.now() <= new Date(event.start_date).getTime() && (
+                <h3 className="text-3xl text-beige-active">
+                  Event starts{" "}
+                  {formatDistanceToNow(new Date(event.start_date), {
+                    addSuffix: true,
+                  })}
+                </h3>
+              )}
+
+              {(Date.now() >= new Date(event.end_date).getTime() && (
+                <h3 className="text-2xl text-beige">
+                  Event ended the{" "}
+                  {new Date(event.end_date).toLocaleDateString()}
+                </h3>
+              )) || (
+                <h3 className="text-2xl text-beige">
+                  Event ends{" "}
+                  {formatDistanceToNow(new Date(event.end_date), {
+                    addSuffix: true,
+                  })}
+                </h3>
+              )}
+
+              <span className="text-beige-inactive">
+                Event period:{" "}
+                {differenceInDays(
+                  new Date(event.end_date),
+                  new Date(event.start_date)
+                )}{" "}
+                days
+              </span>
+            </Lines>
           </div>
 
           <article className="relative py-8 border border-opacity-40 px-8">
