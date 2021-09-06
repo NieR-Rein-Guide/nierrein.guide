722fb107590cfc85dac046e2d67a114402d66efb
Nemesis <keziah.moselle@gmail.com>
2021-09-06 15:58:39 +0200

    fix: tierlist costume toggle responsive

--- src/components/tierlist/TierList.tsx
+++ src/components/tierlist/TierList.tsx
@@ -1,59 +1,59 @@
-import Image from 'next/image'
-import { RANK_THUMBNAILS, TiersTabs } from '@models/tiers'
-import Link from 'next/link'
-import { formatDistanceToNow } from 'date-fns'
-import ReactTooltip from 'react-tooltip'
-import { useState } from 'react'
-import classNames from 'classnames'
-import { FiBarChart2 } from 'react-icons/fi'
-import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts'
-import { useEffect } from 'react'
-import slugify from 'slugify'
+import Image from "next/image";
+import { RANK_THUMBNAILS, TiersTabs } from "@models/tiers";
+import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
+import ReactTooltip from "react-tooltip";
+import { useState } from "react";
+import classNames from "classnames";
+import { FiBarChart2 } from "react-icons/fi";
+import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from "recharts";
+import { useEffect } from "react";
+import slugify from "slugify";
 
 interface TierListProps {
-  tier: TiersTabs
+  tier: TiersTabs;
 }
 
-function findMaxStat(lists, property: 'atk' | 'def' | 'agility') {
+function findMaxStat(lists, property: "atk" | "def" | "agility") {
   return lists.reduce((acc, [, list]) => {
     const max = list.reduce((acc, costume) => {
-      const stat = costume.points[property]
+      const stat = costume.points[property];
 
       if (acc >= stat) {
-        return acc
+        return acc;
       }
 
-      return stat
-    }, 0)
+      return stat;
+    }, 0);
 
     if (acc >= max) {
-      return acc
+      return acc;
     }
 
-    return max
-  }, 0)
+    return max;
+  }, 0);
 }
 
 export default function TierList({ tier }: TierListProps): JSX.Element {
-  const [isStatsEnabled, setIsStatsEnabled] = useState(false)
-  const [maxAtk, setMaxAtk] = useState(10000)
-  const [maxDef, setMaxDef] = useState(10000)
-  const [maxAgi, setMaxAgi] = useState(10000)
-  const lists = Object.entries(tier.tiers)
+  const [isStatsEnabled, setIsStatsEnabled] = useState(false);
+  const [maxAtk, setMaxAtk] = useState(10000);
+  const [maxDef, setMaxDef] = useState(10000);
+  const [maxAgi, setMaxAgi] = useState(10000);
+  const lists = Object.entries(tier.tiers);
 
-  const hasStats = lists?.[0]?.[1]?.[0]?.points
+  const hasStats = lists?.[0]?.[1]?.[0]?.points;
 
   useEffect(() => {
     if (!isStatsEnabled) {
-      return
+      return;
     }
 
-    setMaxAtk(findMaxStat(lists, 'atk'))
-    setMaxDef(findMaxStat(lists, 'def'))
-    setMaxAgi(findMaxStat(lists, 'agility'))
+    setMaxAtk(findMaxStat(lists, "atk"));
+    setMaxDef(findMaxStat(lists, "def"));
+    setMaxAgi(findMaxStat(lists, "agility"));
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isStatsEnabled])
+  }, [isStatsEnabled]);
 
   if (lists.length === 0) {
     return (
@@ -64,17 +64,17 @@ export default function TierList({ tier }: TierListProps): JSX.Element {
           </p>
         </div>
 
-        <p className='text-3xl mb-4'>Work In Progress</p>
+        <p className="text-3xl mb-4">Work In Progress</p>
         {tier.content && (
           <div dangerouslySetInnerHTML={{ __html: tier.content }}></div>
         )}
       </div>
-    )
+    );
   }
 
   return (
-    <div className='flex flex-col gap-y-8 relative'>
-      <div className='flex justify-between pb-8'>
+    <div className="flex flex-col gap-y-8 relative">
+      <div className="flex flex-col md:flex-row justify-between pb-8">
         <div>
           <p>
             Last updated: {formatDistanceToNow(new Date(tier.lastUpdated))} ago
@@ -82,44 +82,44 @@ export default function TierList({ tier }: TierListProps): JSX.Element {
         </div>
 
         {hasStats && (
-          <div className='flex gap-x-4'>
-            <div className='w-28'>
-              <p className='flex items-center gap-x-2'>
-                <span className='w-4 h-4 bg-red-400 inline-block'></span> ATK
+          <div className="flex flex-row-reverse md:flex-row flex-wrap justify-center mt-4 md:mt-0 gap-4">
+            <div className="w-full md:w-28 order-2 md:order-1">
+              <p className="flex items-center gap-x-2">
+                <span className="w-4 h-4 bg-red-400 inline-block"></span> ATK
               </p>
-              <p className='flex items-center gap-x-2'>
-                <span className='w-4 h-4 bg-blue-400 inline-block'></span> DEF
+              <p className="flex items-center gap-x-2">
+                <span className="w-4 h-4 bg-blue-400 inline-block"></span> DEF
               </p>
-              <p className='flex items-center gap-x-2'>
-                <span className='w-4 h-4 bg-green-400 inline-block'></span>{' '}
+              <p className="flex items-center gap-x-2">
+                <span className="w-4 h-4 bg-green-400 inline-block"></span>{" "}
                 AGILITY
               </p>
             </div>
 
-            <label className='flex items-center cursor-pointer w-96'>
+            <label className="flex items-center cursor-pointer w-96 order-1 md:order-2">
               <li
                 className={classNames(
-                  'inline-flex items-center py-6 px-8 rounded-sm w-full relative transition hover:bg-grey-dark border border-beige-inactive',
-                  isStatsEnabled ? 'bg-grey-dark' : 'bg-grey-foreground'
+                  "inline-flex items-center py-6 px-8 rounded-sm w-full relative transition hover:bg-grey-dark border border-beige-inactive",
+                  isStatsEnabled ? "bg-grey-dark" : "bg-grey-foreground"
                 )}
               >
                 <input
-                  type='checkbox'
-                  className='cursor-pointer'
+                  type="checkbox"
+                  className="cursor-pointer"
                   checked={isStatsEnabled}
                   onChange={() => setIsStatsEnabled(!isStatsEnabled)}
                 />
-                <div className='ml-6 inline pr-6'>Show costume statistics</div>
+                <div className="ml-6 inline pr-6">Show costume statistics</div>
 
-                <FiBarChart2 className='absolute right-8 top-1/2 transform -translate-y-1/2 h-8 w-8' />
+                <FiBarChart2 className="absolute right-8 top-1/2 transform -translate-y-1/2 h-8 w-8" />
 
                 <img
                   className={classNames(
-                    'absolute h-16 left-2 transform scale-90 ease-out-cubic transition-all',
-                    isStatsEnabled ? 'opacity-100 scale-100' : 'opacity-0'
+                    "absolute h-16 left-2 transform scale-90 ease-out-cubic transition-all",
+                    isStatsEnabled ? "opacity-100 scale-100" : "opacity-0"
                   )}
-                  src='/ui/costume_emblem/costume_emblem099_full.png'
-                  alt='Checked'
+                  src="/ui/costume_emblem/costume_emblem099_full.png"
+                  alt="Checked"
                 />
               </li>
             </label>
@@ -128,33 +128,33 @@ export default function TierList({ tier }: TierListProps): JSX.Element {
       </div>
 
       {lists.map(([rank, items]) => (
-        <div className='tierlist__row' key={rank}>
+        <div className="tierlist__row" key={rank}>
           <Image src={RANK_THUMBNAILS[rank]} alt={rank} />
 
-          <div className='flex flex-wrap justify-center md:justify-start gap-4'>
+          <div className="flex flex-wrap justify-center md:justify-start gap-4">
             {items.map((item, index) => (
-              <div key={`${rank}-${item.name}-${index}`} className='relative'>
+              <div key={`${rank}-${item.name}-${index}`} className="relative">
                 {item.tooltip && (
                   <>
                     <div
                       data-tip
                       data-for={`${rank}-${item.name}-${index}`}
-                      className='absolute -top-2 right-2 bg-white text-black h-6 w-6 flex justify-center items-center rounded-full z-20'
+                      className="absolute -top-2 right-2 bg-white text-black h-6 w-6 flex justify-center items-center rounded-full z-20"
                     >
                       <span>?</span>
                     </div>
 
                     <ReactTooltip
                       id={`${rank}-${item.name}-${index}`}
-                      aria-haspopup='true'
-                      className='tierlist-tooltip'
-                      effect='solid'
+                      aria-haspopup="true"
+                      className="tierlist-tooltip"
+                      effect="solid"
                       delayHide={500}
-                      place='top'
+                      place="top"
                       multiline
                     >
                       <div
-                        className='max-w-xs lg:max-w-md'
+                        className="max-w-xs lg:max-w-md"
                         dangerouslySetInnerHTML={{ __html: item.tooltip }}
                       ></div>
                     </ReactTooltip>
@@ -162,53 +162,53 @@ export default function TierList({ tier }: TierListProps): JSX.Element {
                 )}
                 <Link
                   href={`/characters/${item.name}${
-                    item.title ? '/' + slugify(item.title) : ''
+                    item.title ? "/" + slugify(item.title) : ""
                   }`}
                   passHref={true}
                 >
-                  <a className='flex flex-col items-center gap-y-2 w-28 transform transition-transform ease-out-cubic hover:-translate-y-1'>
+                  <a className="flex flex-col items-center gap-y-2 w-28 transform transition-transform ease-out-cubic hover:-translate-y-1">
                     <img
-                      height='80'
-                      width='80'
+                      height="80"
+                      width="80"
                       src={item.thumb}
                       alt={`${item.name} thumbnail`}
                     />
-                    <span className='text-center font-mono'>
-                      {item.isEX && <span className='text-rarity-4'>EX </span>}
+                    <span className="text-center font-mono">
+                      {item.isEX && <span className="text-rarity-4">EX </span>}
                       {item.name}
                     </span>
                   </a>
                 </Link>
 
                 {isStatsEnabled && item.points && (
-                  <div className='w-28 my-2 text-center'>
-                    <span className='text-sm'>
+                  <div className="w-28 my-2 text-center">
+                    <span className="text-sm">
                       {Math.round(item.points.total)} points
                     </span>
-                    <div className='h-6'>
-                      <ResponsiveContainer width='100%' height='100%'>
-                        <BarChart layout='vertical' data={[item.points]}>
-                          <XAxis type='number' hide domain={[0, maxAtk]} />
-                          <YAxis type='category' width={1} dataKey='name' />
-                          <Bar dataKey='atk' fill='#F87171' />
+                    <div className="h-6">
+                      <ResponsiveContainer width="100%" height="100%">
+                        <BarChart layout="vertical" data={[item.points]}>
+                          <XAxis type="number" hide domain={[0, maxAtk]} />
+                          <YAxis type="category" width={1} dataKey="name" />
+                          <Bar dataKey="atk" fill="#F87171" />
                         </BarChart>
                       </ResponsiveContainer>
                     </div>
-                    <div className='h-6'>
-                      <ResponsiveContainer width='100%' height='100%'>
-                        <BarChart layout='vertical' data={[item.points]}>
-                          <XAxis type='number' hide domain={[0, maxDef]} />
-                          <YAxis type='category' width={1} dataKey='name' />
-                          <Bar dataKey='def' fill='#60A5FA' />
+                    <div className="h-6">
+                      <ResponsiveContainer width="100%" height="100%">
+                        <BarChart layout="vertical" data={[item.points]}>
+                          <XAxis type="number" hide domain={[0, maxDef]} />
+                          <YAxis type="category" width={1} dataKey="name" />
+                          <Bar dataKey="def" fill="#60A5FA" />
                         </BarChart>
                       </ResponsiveContainer>
                     </div>
-                    <div className='h-6'>
-                      <ResponsiveContainer width='100%' height='100%'>
-                        <BarChart layout='vertical' data={[item.points]}>
-                          <XAxis type='number' hide domain={[0, maxAgi]} />
-                          <YAxis type='category' width={1} dataKey='name' />
-                          <Bar dataKey='agility' fill='#34D399' />
+                    <div className="h-6">
+                      <ResponsiveContainer width="100%" height="100%">
+                        <BarChart layout="vertical" data={[item.points]}>
+                          <XAxis type="number" hide domain={[0, maxAgi]} />
+                          <YAxis type="category" width={1} dataKey="name" />
+                          <Bar dataKey="agility" fill="#34D399" />
                         </BarChart>
                       </ResponsiveContainer>
                     </div>
@@ -218,9 +218,9 @@ export default function TierList({ tier }: TierListProps): JSX.Element {
             ))}
           </div>
           <img
-            className='py-8 w-full col-span-full opacity-20'
-            src='./border.svg'
-            alt=''
+            className="py-8 w-full col-span-full opacity-20"
+            src="./border.svg"
+            alt=""
           />
         </div>
       ))}
@@ -233,10 +233,10 @@ export default function TierList({ tier }: TierListProps): JSX.Element {
 
       {tier.content && (
         <div
-          className='darken-img'
+          className="darken-img"
           dangerouslySetInnerHTML={{ __html: tier.content }}
         ></div>
       )}
     </div>
-  )
+  );
 }
