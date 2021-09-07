5bc4175e56a18be3ac325985238daa6e2401cefd
Nemesis <keziah.moselle@gmail.com>
2021-09-07 20:15:28 +0200

    chore: fix pipeline

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,9 @@
-import { Costume } from "@models/character";
+import { Costume } from "@models/types";
 import Link from "next/link";
 import Image from "next/image";
 import slugify from "slugify";
 import Squares from "./decorations/Squares";
+import RARITY from "@utils/rarity";
 export { Card, CharacterCard };
 
 function Card(): JSX.Element {
@@ -12,34 +13,40 @@ function Card(): JSX.Element {
 function CharacterCard({ costume }: { costume: Costume }): JSX.Element {
   return (
     <Link
-      href={`/characters/${slugify(`${costume.character}/${costume.name.en}`, {
-        remove: /[*+~.()'"!:@]/g,
-      })}`}
+      href={`/characters/${slugify(
+        `${costume.character}/${costume.costume.name.en}`,
+        {
+          remove: /[*+~.()'"!:@]/g,
+        }
+      )}`}
     >
       {/* Eventually replace with this */}
       {/* ${slugify(`${costume.character}/${costume.name.en}`, { lower: true, remove: /[*+~.()'"!:@]/g })}`}> */}
       <a>
-        <div className={`card rarity${costume.stars}`}>
+        <div className={`card rarity${RARITY[costume.costume.rarity]}`}>
           <div className="card-art">
             <div className="squares-container">
               <Squares />
             </div>
             <div className="weapon-and-element-container">
-              <img src="/icons/elements/dark.png"></img>
-              <img src="/icons/weapons/gun.png"></img>
+              <img src="/icons/elements/dark.png" alt=""></img>
+              <img src="/icons/weapons/gun.png" alt=""></img>
             </div>
             <div className="star-container">
-              {Array.from(Array(costume.stars), (e, i) => {
+              {Array.from(Array(RARITY[costume.costume.rarity]), () => {
                 return (
                   <img
-                    src={`/icons/stars/${costume.stars}_star_single.svg`}
+                    src={`/icons/stars/${
+                      RARITY[costume.costume.rarity]
+                    }_star_single.svg`}
+                    alt=""
                   ></img>
                 );
               })}
             </div>
             <Image
-              src={costume.illustrationURL}
-              alt={`${costume.character} (${costume.name.en}) illustration`}
+              src={`/character_medium/${costume.ids.actor}_full-1920-1080.png`}
+              alt={`${costume.character} (${costume.costume.name.en}) illustration`}
               className="card-art"
               layout="fill"
             />
