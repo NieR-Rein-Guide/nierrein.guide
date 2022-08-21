b97f194103bdcb4f04d6b4a8cfc1f185732b1194
Nemesis <keziah.moselle@gmail.com>
2022-08-21 13:33:49 +0200

    feat: memoirs

--- src/pages/database/memoirs.tsx
+++ src/pages/database/memoirs.tsx
@@ -1,19 +1,21 @@
 import Layout from "@components/Layout";
 import Meta from "@components/Meta";
-import { getAllMemoirs } from "@models/memoir";
 import Link from "next/link";
 import Image from "next/image";
 import SVG from "react-inlinesvg";
+import prisma from "@libs/prisma";
+import { memoir, memoir_series } from "@prisma/client";
+import { CDN_URL } from "@config/constants";
 
 interface DatabaseMemoirsProps {
-  allMemoirs: string;
+  memoirs: (memoir_series & {
+    memoir: memoir[];
+  })[];
 }
 
-export default function Databaseweapons({
-  allMemoirs,
+export default function MemoirsPage({
+  memoirs,
 }: DatabaseMemoirsProps): JSX.Element {
-  const memoirs = JSON.parse(allMemoirs);
-
   return (
     <Layout>
       <Meta
@@ -35,29 +37,23 @@ export default function Databaseweapons({
         <h2 className="overlap">Memoirs</h2>
 
         {memoirs.map((memoir) => (
-          <div className="mb-8" key={memoir.MemoirSeriesId}>
+          <div className="mb-8" key={memoir.memoir_series_id}>
             <h3 className="text-beige text-3xl">
-              {memoir.abilities[1].description}
+              {memoir.large_set_description}
             </h3>
             <span className="text-sm text-beige-text">
-              Small set: {memoir.abilities[0].description}
+              Small set: {memoir.small_set_description}
             </span>
 
-            <div className="flex flex-wrap gap-x-16 mt-4">
-              {spliceIntoChunks(memoir.memoirs, 3).map((set) => (
-                <div className="flex gap-x-2 mt-2" key={set[0].MemoirId}>
-                  {set.map((memoir) => (
-                    <div key={memoir.MemoirId}>
-                      <Image
-                        width={96}
-                        height={96}
-                        src={`/ui/memory/memory${String(
-                          memoir.MemoirId
-                        ).padStart(3, "0")}_full.png`}
-                        alt=""
-                      />
-                    </div>
-                  ))}
+            <div className="flex gap-2 mt-4">
+              {memoir.memoir.map((memoir) => (
+                <div className="relative" key={memoir.memoir_id}>
+                  <Image
+                    width={96}
+                    height={96}
+                    src={`${CDN_URL}${memoir.image_path_base}full.png`}
+                    alt=""
+                  />
                 </div>
               ))}
             </div>
@@ -68,21 +64,26 @@ export default function Databaseweapons({
   );
 }
 
-function spliceIntoChunks(arr, chunkSize) {
-  const res = [];
-  while (arr.length > 0) {
-    const chunk = arr.splice(0, chunkSize);
-    res.push(chunk);
-  }
-  return res;
-}
-
 export async function getStaticProps() {
-  const { series } = await getAllMemoirs();
+  const memoirs = await prisma.memoir_series.findMany({
+    orderBy: {
+      memoir_series_id: "asc",
+    },
+    include: {
+      memoir: {
+        where: {
+          lottery_id: 5,
+          rarity: "SS_RARE",
+        },
+      },
+    },
+  });
 
   return {
-    props: {
-      allMemoirs: JSON.stringify(series),
-    },
+    props: JSON.parse(
+      JSON.stringify({
+        memoirs,
+      })
+    ),
   };
 }
