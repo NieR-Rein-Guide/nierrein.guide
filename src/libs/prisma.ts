0e10ee2c5a06f09f2193fd60458a01b1bfa320b4
Nemesis <keziah.moselle@gmail.com>
2022-08-26 00:53:43 +0200

    chore: fix build?

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -8,21 +8,22 @@ declare global {
   namespace NodeJS {
     interface Global {
       prisma: PrismaDump;
-      nrgprisma: PrismaNRG;
     }
   }
 }
 
-let prisma: PrismaDump = global.prisma || new PrismaDump({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    }
-  },
-  log: ["info"],
-})
+if (!global.prisma) {
+  global.prisma = new PrismaDump({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      }
+    },
+    log: ["info"],
+  })
+}
 
-export let nrgprisma: PrismaNRG = global.nrgprisma || new PrismaNRG({
+export const nrgprisma = new PrismaNRG({
   datasources: {
     db: {
       url: process.env.NIERREINGUIDE_DATABASE_URL,
@@ -31,7 +32,4 @@ export let nrgprisma: PrismaNRG = global.nrgprisma || new PrismaNRG({
   log: ["info"],
 })
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
-if (process.env.NODE_ENV !== 'production') global.nrgprisma = nrgprisma
-
-export default prisma;
+export default global.prisma;
