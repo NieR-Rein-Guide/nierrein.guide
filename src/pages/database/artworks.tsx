096d154b74501e10a8c4e3b5fb7818f4167328d6
Nemesis <contact@keziahmoselle.fr>
2022-09-08 15:32:21 +0200

    chore: maintenance artworks database

--- src/pages/database/artworks.tsx
+++ src/pages/database/artworks.tsx
@@ -1,20 +1,10 @@
-import { readdir } from "fs/promises";
-import { resolve } from "path";
 import Layout from "@components/Layout";
 import Meta from "@components/Meta";
 import Link from "next/link";
 import SVG from "react-inlinesvg";
 import Image from "next/image";
 
-interface DatabaseStoriesProps {
-  characters: string[];
-  thumbnails: string[];
-}
-
-export default function DatabaseStories({
-  characters,
-  thumbnails,
-}: DatabaseStoriesProps): JSX.Element {
+export default function DatabaseStories(): JSX.Element {
   return (
     <Layout>
       <Meta
@@ -32,56 +22,19 @@ export default function DatabaseStories({
         </Link>
       </nav>
 
-      <section>
-        <h2 className="overlap">Assets Artworks</h2>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-8 lg:gap-20">
-          {thumbnails.map((thumbnail, index) => (
-            <div className="h-96 w-auto relative" key={thumbnail}>
-              <Link href={`/character/${characters[index]}`} passHref={true}>
-                <a>
-                  <Image
-                    layout="fill"
-                    objectFit="contain"
-                    objectPosition="center"
-                    src={`/character_thumbnails/${thumbnail}`}
-                    alt={`${thumbnail}`}
-                  />
-                </a>
-              </Link>
-            </div>
-          ))}
+      <div className="bg-grey-dark text-beige transition-colors w-full border-b border-beige-inactive border-opacity-50 p-8 text-center rounded-lg">
+        <img
+          className="inline-block"
+          src="/decorations/fio-confused.png"
+          alt="Fio confused"
+        />
+        <p className="mt-4">Sorry, this page is being reworked.</p>
+        <div className="flex justify-center mt-4">
+          <Link href="/characters" passHref>
+            <a className="btn">Go to characters page to see artworks</a>
+          </Link>
         </div>
-      </section>
+      </div>
     </Layout>
   );
-}
-
-export async function getStaticProps() {
-  const charactersFolder = resolve(process.cwd(), "public", "character");
-  const thumbnailsCharactersFolder = resolve(
-    process.cwd(),
-    "public",
-    "character_thumbnails"
-  );
-
-  const [characters, thumbnails] = await Promise.all([
-    readdir(charactersFolder),
-    readdir(thumbnailsCharactersFolder),
-  ]);
-
-  const filteredCharacters = characters.filter(
-    (file) => file.endsWith(".png") || file.endsWith(".jpg")
-  );
-
-  const filteredThumbnails = thumbnails.filter(
-    (file) => file.endsWith(".png") || file.endsWith(".jpg")
-  );
-
-  return {
-    props: {
-      characters: filteredCharacters,
-      thumbnails: filteredThumbnails,
-    },
-  };
 }
