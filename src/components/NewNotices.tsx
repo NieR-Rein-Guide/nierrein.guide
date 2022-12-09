85dcacf0d24bb05a10b5c5086ca2f0102e9c2633
Nemesis <contact@keziahmoselle.fr>
2022-12-09 21:54:10 +0100

    feat(notices): improve notices ui on homepage

--- src/components/NewNotices.tsx
+++ src/components/NewNotices.tsx
@@ -2,50 +2,84 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { A11y, Pagination, Navigation } from "swiper";
 import Link from "next/link";
 import useBreakpoint from "@hooks/useBreakpoint";
+import classNames from "classnames";
+
+const SLIDER_BREAKPOINTS = ["lg", "xl", "2xl"];
 
 export default function NewNotices({ notifications }) {
   const breakpoint = useBreakpoint();
 
   console.log(breakpoint);
   return (
-    <div className="swiper--notices overflow-hidden relative pb-16">
-      <Swiper
-        modules={[A11y, Pagination, Navigation]}
-        slidesPerView={
-          (breakpoint === "sm" && 2) ||
-          (breakpoint === "md" && 3) ||
-          (breakpoint === "lg" && 5) ||
-          (breakpoint === "xl" && 5) ||
-          (breakpoint === "2xl" && 5)
-        }
-        pagination={{ clickable: true }}
-        navigation
-        className="cursor-move"
-      >
-        {notifications.map((notification) => (
-          <SwiperSlide key={notification.notification_id}>
-            <div className="relative flex items-center justify-center h-28 w-52 bg-grey-dark border border-beige border-opacity-50">
-              {(notification.thumbnail_path && (
-                <img
-                  className="object-contain mt-4 mb-2 md:mt-0 md:mb-0"
-                  loading="lazy"
-                  src={`https://web.app.nierreincarnation.com${notification.thumbnail_path}`}
-                  alt={notification.title}
-                />
-              )) || (
-                <h3 className="text-2xl text-center line-clamp-3">
-                  {notification.title}
-                </h3>
-              )}
-              <Link href={`/notice/${notification.notification_id}`} passHref>
-                <a className="absolute inset-0 z-10">
-                  <span className="sr-only">See notice</span>
-                </a>
-              </Link>
-            </div>
-          </SwiperSlide>
+    <>
+      <div className="swiper--notices overflow-hidden relative pb-16">
+        {SLIDER_BREAKPOINTS.includes(breakpoint) && (
+          <Swiper
+            modules={[A11y, Pagination, Navigation]}
+            slidesPerView={5}
+            pagination={{ clickable: true }}
+            navigation
+            className="cursor-move"
+          >
+            {notifications.map((notification) => (
+              <SwiperSlide key={notification.notification_id}>
+                <div className="relative flex items-center justify-center h-28 w-52 bg-grey-dark border border-beige border-opacity-50">
+                  {(notification.thumbnail_path && (
+                    <img
+                      className="object-contain mt-4 mb-2 md:mt-0 md:mb-0"
+                      loading="lazy"
+                      src={`https://web.app.nierreincarnation.com${notification.thumbnail_path}`}
+                      alt={notification.title}
+                    />
+                  )) || (
+                    <h3 className="text-2xl text-center line-clamp-3">
+                      {notification.title}
+                    </h3>
+                  )}
+                  <Link
+                    href={`/notice/${notification.notification_id}`}
+                    passHref
+                  >
+                    <a className="absolute inset-0 z-10">
+                      <span className="sr-only">See notice</span>
+                    </a>
+                  </Link>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+      </div>
+
+      <div className="flex flex-wrap gap-x-2 justify-center lg:hidden">
+        {[...notifications].splice(0, 8).map((notification) => (
+          <div
+            key={notification.notification_id}
+            className={classNames(
+              "relative flex items-center justify-center h-28 w-52 bg-grey-dark border border-beige border-opacity-50",
+              notification.thumbnail_path ? "" : "p-4"
+            )}
+          >
+            {(notification.thumbnail_path && (
+              <img
+                className="object-contain mt-4 mb-2 md:mt-0 md:mb-0"
+                loading="lazy"
+                src={`https://web.app.nierreincarnation.com${notification.thumbnail_path}`}
+                alt={notification.title}
+              />
+            )) || (
+              <h3 className="text-xl text-center line-clamp-3">
+                {notification.title}
+              </h3>
+            )}
+            <Link href={`/notice/${notification.notification_id}`} passHref>
+              <a className="absolute inset-0 z-10">
+                <span className="sr-only">See notice</span>
+              </a>
+            </Link>
+          </div>
         ))}
-      </Swiper>
-    </div>
+      </div>
+    </>
   );
 }
