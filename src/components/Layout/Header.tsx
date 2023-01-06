c2de6e782559ba31b0aeb6f620d667ddc340ea6f
Nemesis <contact@keziahmoselle.fr>
2023-01-06 18:40:51 +0100

    feat: add kofi button in header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -10,8 +10,11 @@ import SVG from "react-inlinesvg";
 import axios from "axios";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import SettingsModal from "@components/SettingsModal";
+import { Modal } from "@mui/material";
+import { BiDonateHeart } from "react-icons/bi";
 
 export default function Header(): JSX.Element {
+  const [isWhySupportModalOpen, setIsWhySupportModalOpen] = useState(false);
   const [isNavOpened, setIsNavOpened] = useState(false);
   const router = useRouter();
   const defaultAnimationPosition = 18;
@@ -62,6 +65,78 @@ export default function Header(): JSX.Element {
     <>
       <div className="absolute xl:fixed right-0 left-0 top-0 mx-auto z-menu">
         <div className="relative flex justify-start md:justify-center items-center gap-x-2 px-4 py-2 bg-grey-lighter text-beige transition-colors w-full border-b border-beige-inactive border-opacity-50">
+          <div className="hidden absolute left-4 md:flex items-center">
+            <a
+              className=""
+              href="https://ko-fi.com/A0A35V520"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                height="36"
+                className="border-none h-8"
+                src="https://storage.ko-fi.com/cdn/kofi1.png?v=3"
+                alt="Buy Me a Coffee at ko-fi.com"
+              />
+            </a>
+
+            <button
+              onClick={() => setIsWhySupportModalOpen(true)}
+              className="inline-block ml-2 underline text-xs"
+            >
+              why?
+            </button>
+
+            <Modal
+              open={isWhySupportModalOpen}
+              onClose={() => setIsWhySupportModalOpen(false)}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <div className="bg-grey-dark p-8 absolute border border-beige border-opacity-50 top-0 left-0 md:top-1/2 md:left-1/2 transform md:-translate-x-1/2 md:-translate-y-1/2 w-full md:max-w-3xl space-y-8 overflow-y-auto pt-12 md:pt-8 max-h-screen">
+                <div className="flex items-center justify-between mb-4 pt-12">
+                  <h3 className="flex items-center gap-x-2 text-3xl">
+                    <BiDonateHeart /> Why donate?
+                  </h3>
+                  <button
+                    className="btn"
+                    onClick={() => setIsWhySupportModalOpen(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+
+                <p>
+                  I have been maintaining this website since August 18 2021, and
+                  didn't want to put ads since then.
+                </p>
+                <p>
+                  I don't mind paying for the hosting of all the services that
+                  make this site work but these last months are particularly
+                  complicated for me and I would like to reduce the expenses of
+                  my personal projects, that's why I'm putting this ko-fi link
+                  publicly as of now.
+                </p>
+                <p>
+                  I am sorry if that bothers some of you. Hope the website is
+                  useful to you! {"<3"}
+                </p>
+                <p>Here are some expenses for all services I maintain:</p>
+                <p className="font-display text-xl">
+                  Monthly hosting expense (total includes other side projects)
+                </p>
+                <img
+                  src="/images/donating-modal/railway.png"
+                  alt="Railway"
+                ></img>
+                <p className="font-display text-xl">
+                  Monthly assets expense (including 3D models)
+                </p>
+                <img src="/images/donating-modal/wasabi.png" alt="Wasabi"></img>
+              </div>
+            </Modal>
+          </div>
+
           <span className="text-xs md:text-base">
             {(release && (
               <Link
