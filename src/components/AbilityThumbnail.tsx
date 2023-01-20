4f801cd04f21bf0f525e1d5453e1f412393b61df
Nemesis <contact@keziahmoselle.fr>
2023-01-20 18:07:56 +0100

    feat: more area to hover ability and skill description

--- src/components/AbilityThumbnail.tsx
+++ src/components/AbilityThumbnail.tsx
@@ -15,31 +15,31 @@ export default function AbilityThumbnail({
   return (
     <div className="relative flex flex-col justify-center items-center">
       <div className="relative transform">
-        {hasTooltip && (
-          <Tooltip
-            className="cursor-help"
-            title={
-              <div
-                className="text-center"
-                dangerouslySetInnerHTML={{
-                  __html: ability.description,
-                }}
-              ></div>
-            }
-          >
+        <Tooltip
+          className="cursor-help"
+          title={
+            <div
+              className="text-center"
+              dangerouslySetInnerHTML={{
+                __html: ability.description,
+              }}
+            ></div>
+          }
+        >
+          <div>
+            <SVG src="/decorations/frame-ability.svg" className="h-12 w-12" />
+            <div className="h-12 w-12 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+              <img
+                className="h-6"
+                alt=""
+                src={`${CDN_URL}${ability.image_path_base}standard.png`}
+              />
+            </div>
             <div className="absolute -top-2 -right-2 z-20 flex items-center justify-center bg-grey-lighter text-beige border border-beige rounded-full h-5 w-5 font-labor">
               ?
             </div>
-          </Tooltip>
-        )}
-        <SVG src="/decorations/frame-ability.svg" className="h-12 w-12" />
-        <div className="h-12 w-12 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-          <img
-            className="h-6"
-            alt=""
-            src={`${CDN_URL}${ability.image_path_base}standard.png`}
-          />
-        </div>
+          </div>
+        </Tooltip>
       </div>
 
       {children || (
--- src/components/SkillThumbnail.tsx
+++ src/components/SkillThumbnail.tsx
@@ -15,34 +15,38 @@ export default function SkillThumbnail({
   return (
     <div className="relative flex flex-col justify-center items-center">
       <div className="relative transform">
-        {hasTooltip && (
-          <Tooltip
-            className="cursor-help"
-            title={
-              <div
-                className="text-center"
-                dangerouslySetInnerHTML={{
-                  __html: skill.description,
-                }}
-              ></div>
-            }
-          >
+        <Tooltip
+          className="cursor-help"
+          title={
+            <div
+              className="text-center"
+              dangerouslySetInnerHTML={{
+                __html: skill.description,
+              }}
+            ></div>
+          }
+        >
+          <div>
+            <SVG
+              src="/decorations/frame-ability.svg"
+              className="h-12 w-12 transform rotate-45"
+            />
+            <div className="h-12 w-12 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+              <img
+                className="h-6"
+                alt=""
+                src={`${CDN_URL}${skill.image_path}`}
+              />
+            </div>
             <div className="absolute -top-2 -right-2 z-20 flex items-center justify-center bg-grey-lighter text-beige border border-beige rounded-full h-5 w-5 font-labor">
               ?
             </div>
-          </Tooltip>
-        )}
-        <SVG
-          src="/decorations/frame-ability.svg"
-          className="h-12 w-12 transform rotate-45"
-        />
-        <div className="h-12 w-12 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-          <img className="h-6" alt="" src={`${CDN_URL}${skill.image_path}`} />
-        </div>
+          </div>
+        </Tooltip>
       </div>
 
       {children || (
-        <span className="mt-1 text-sm text-center leading-none">
+        <span className="mt-2 text-sm text-center leading-none">
           {skill.name}
         </span>
       )}
