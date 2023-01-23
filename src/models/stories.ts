4b4f02daab228870635c52427a35067a1bc35ba8
Nemesis <contact@keziahmoselle.fr>
2023-01-23 18:46:53 +0100

    refactor(strapi): get story

--- src/models/stories.ts
+++ src/models/stories.ts
@@ -15,7 +15,7 @@ async function getStory(slug: string): Promise<Story> {
   const { data } = await axios.get(
     `${env.NEXT_PUBLIC_STRAPI_REST_API_ENDPOINT}/lores?filters[slug][$eqi]=${slug}&populate=*`);
 
-  const story: Story = data.data;
+  const story: Story = data.data[0];
 
   return story;
 }
