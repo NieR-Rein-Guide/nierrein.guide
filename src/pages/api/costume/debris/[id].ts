b7b74661c4d5398e71af47cc93388e489c4096ca
Nemesis <contact@keziahmoselle.fr>
2023-01-23 21:40:52 +0100

    feat(api): update debris to use the database instead of manual link

--- src/pages/api/costume/debris/[id].ts
+++ src/pages/api/costume/debris/[id].ts
@@ -1,12 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@libs/prisma";
 
-export interface CostumeLink {
-  costume_id?: number;
-  weapon_id?: number;
-  debris_id?: number;
-}
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query
 
@@ -18,19 +12,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         })
       }
 
-      const link = await prisma.nrg.costumes_link.findUnique({
+      const costume = await prisma.dump.costume.findUnique({
         where: {
-          costume_id: Number(id),
+          costume_id: Number(id)
+        },
+        include: {
+          debris: true,
         }
       })
 
-      const debris = await prisma.dump.debris.findUnique({
-        where: {
-          debris_id: link.debris_id,
-        }
-      })
-
-      return res.status(200).json(debris)
+      return res.status(200).json(costume.debris)
     } catch (error) {
       console.error(error)
       return res.status(500).json({
