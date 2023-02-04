d01f4afbb228f0f334b1bb51bd41c936367cdf2b
Nemesis <contact@keziahmoselle.fr>
2023-02-04 03:55:45 +0100

    feat: update error message

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -17,10 +17,11 @@ export default class ErrorBoundary extends React.Component {
   render(): React.ReactNode {
     if (this.state.hasError) {
       console.log(this.state.error);
-      // You can render any custom fallback UI
+
       return (
-        <div className="flex flex-col items-center justify-center mt-12">
+        <div className="flex flex-col items-center justify-center mt-12 text-center">
           <p>Something went wrong.</p>
+          <h3>Please try refreshing the page.</h3>
         </div>
       );
     }
