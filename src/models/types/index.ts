93babe6aa7a0dc90b8942eb4a4fe0f0889ea580a
Nemesis <contact@keziahmoselle.fr>
2023-02-04 23:39:31 +0100

    chore: lint

--- src/models/types/index.ts
+++ src/models/types/index.ts
@@ -10,29 +10,29 @@ export interface StrapiImageFormats {
 export type Poll = {
   title: string;
   embed: string;
-}
+};
 
 export type StrapiImage = {
-  name:              string;
-  alternativeText:   string;
-  caption:           string;
-  width:             number;
-  height:            number;
-  formats:           FluffyFormats;
-  hash:              string;
-  ext:               EXT;
-  mime:              MIME;
-  size:              number;
-  url:               string;
-  previewUrl:        null;
-  provider:          string;
+  name: string;
+  alternativeText: string;
+  caption: string;
+  width: number;
+  height: number;
+  formats: FluffyFormats;
+  hash: string;
+  ext: EXT;
+  mime: MIME;
+  size: number;
+  url: string;
+  previewUrl: null;
+  provider: string;
   provider_metadata: null;
-  createdAt:         Date;
-  updatedAt:         Date;
-}
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 export type FanContent = {
-  type: 'art' | 'video';
+  type: "art" | "video";
   author?: string;
   link?: string;
   image?: {
@@ -40,48 +40,48 @@ export type FanContent = {
     formats: StrapiImageFormats;
   };
   published_at?: string;
-}
+};
 
 export interface FAQ {
-    id:         number;
-    attributes: FAQAttributes;
+  id: number;
+  attributes: FAQAttributes;
 }
 
 export interface FAQAttributes {
-    createdAt:     Date;
-    updatedAt:     Date;
-    locale:        string;
-    qa:            QA[];
-    localizations: Localizations;
+  createdAt: Date;
+  updatedAt: Date;
+  locale: string;
+  qa: QA[];
+  localizations: Localizations;
 }
 
 export interface QA {
-    id:       number;
-    question: string;
-    answer:   string;
+  id: number;
+  question: string;
+  answer: string;
 }
 
 export interface Event {
-  id:         number;
+  id: number;
   attributes: EventAttributes;
   costumes: (costume & {
     character: character;
-  })[]
+  })[];
 }
 
 export interface EventAttributes {
-  title:         string;
-  createdAt:     Date;
-  updatedAt:     Date;
-  publishedAt:   Date;
-  content:       string;
-  start_date:    Date;
-  end_date:      Date;
-  slug:          string;
-  Type:          null;
-  gems:          null;
-  locale:        string;
-  image:         Image;
+  title: string;
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+  content: string;
+  start_date: Date;
+  end_date: Date;
+  slug: string;
+  Type: null;
+  gems: null;
+  locale: string;
+  image: Image;
   localizations: Localizations;
 }
 
@@ -90,45 +90,45 @@ export interface Image {
 }
 
 export interface ImageData {
-  id:         number;
+  id: number;
   attributes: StrapiImageAttributes;
 }
 
 export interface StrapiImageAttributes {
-  name:              string;
-  alternativeText:   string;
-  caption:           string;
-  width:             number;
-  height:            number;
-  formats:           Formats;
-  hash:              string;
-  ext:               string;
-  mime:              string;
-  size:              number;
-  url:               string;
-  previewUrl:        null;
-  provider:          string;
+  name: string;
+  alternativeText: string;
+  caption: string;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: null;
+  provider: string;
   provider_metadata: null;
-  createdAt:         Date;
-  updatedAt:         Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface Formats {
-  large:     Large;
-  small:     Large;
-  medium:    Large;
+  large: Large;
+  small: Large;
+  medium: Large;
   thumbnail: Large;
 }
 
 export interface Large {
-  ext:    string;
-  url:    string;
-  hash:   string;
-  mime:   string;
-  name:   string;
-  path:   null;
-  size:   number;
-  width:  number;
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: null;
+  size: number;
+  width: number;
   height: number;
 }
 
@@ -137,172 +137,178 @@ export interface Localizations {
 }
 
 export interface Guide {
-    id:         number;
-    attributes: DatumAttributes;
+  id: number;
+  attributes: DatumAttributes;
 }
 
 export interface DatumAttributes {
-    content:       string;
-    title:         string;
-    slug:          string;
-    author:        string;
-    description:   null | string;
-    createdAt:     Date;
-    updatedAt:     Date;
-    publishedAt:   Date;
-    locale:        string;
-    cover:         Cover;
-    thumbnail:     Thumbnail;
-    localizations: Localizations;
+  content: string;
+  title: string;
+  slug: string;
+  author: string;
+  description: null | string;
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+  locale: string;
+  cover: Cover;
+  thumbnail: Thumbnail;
+  localizations: Localizations;
 }
 
 export interface Cover {
-    data: CoverData;
+  data: CoverData;
 }
 
 export interface CoverData {
-    id:         number;
-    attributes: PurpleAttributes;
+  id: number;
+  attributes: PurpleAttributes;
 }
 
 export interface PurpleAttributes {
-    name:              string;
-    alternativeText:   string;
-    caption:           string;
-    width:             number;
-    height:            number;
-    formats:           PurpleFormats;
-    hash:              string;
-    ext:               EXT;
-    mime:              MIME;
-    size:              number;
-    url:               string;
-    previewUrl:        null;
-    provider:          string;
-    provider_metadata: null;
-    createdAt:         Date;
-    updatedAt:         Date;
+  name: string;
+  alternativeText: string;
+  caption: string;
+  width: number;
+  height: number;
+  formats: PurpleFormats;
+  hash: string;
+  ext: EXT;
+  mime: MIME;
+  size: number;
+  url: string;
+  previewUrl: null;
+  provider: string;
+  provider_metadata: null;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export enum EXT {
-    Jpg = ".jpg",
-    PNG = ".png",
+  Jpg = ".jpg",
+  PNG = ".png",
 }
 
 export interface PurpleFormats {
-    large:     Large;
-    small:     Large;
-    medium:    Large;
-    thumbnail: Large;
+  large: Large;
+  small: Large;
+  medium: Large;
+  thumbnail: Large;
 }
 
 export interface Large {
-    ext:    EXT;
-    url:    string;
-    hash:   string;
-    mime:   MIME;
-    name:   string;
-    path:   null;
-    size:   number;
-    width:  number;
-    height: number;
+  ext: EXT;
+  url: string;
+  hash: string;
+  mime: MIME;
+  name: string;
+  path: null;
+  size: number;
+  width: number;
+  height: number;
 }
 
 export enum MIME {
-    ImageJPEG = "image/jpeg",
-    ImagePNG = "image/png",
+  ImageJPEG = "image/jpeg",
+  ImagePNG = "image/png",
 }
 
 export interface Localizations {
-    data: any[];
+  data: any[];
 }
 
 export interface Thumbnail {
-    data: ThumbnailData;
+  data: ThumbnailData;
 }
 
 export interface ThumbnailData {
-    id:         number;
-    attributes: FluffyAttributes;
+  id: number;
+  attributes: FluffyAttributes;
 }
 
 export interface FluffyAttributes {
-    name:              string;
-    alternativeText:   string;
-    caption:           string;
-    width:             number;
-    height:            number;
-    formats:           FluffyFormats;
-    hash:              string;
-    ext:               EXT;
-    mime:              MIME;
-    size:              number;
-    url:               string;
-    previewUrl:        null;
-    provider:          string;
-    provider_metadata: null;
-    createdAt:         Date;
-    updatedAt:         Date;
+  name: string;
+  alternativeText: string;
+  caption: string;
+  width: number;
+  height: number;
+  formats: FluffyFormats;
+  hash: string;
+  ext: EXT;
+  mime: MIME;
+  size: number;
+  url: string;
+  previewUrl: null;
+  provider: string;
+  provider_metadata: null;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface FluffyFormats {
-    small?:    Large;
-    thumbnail: Large;
+  small?: Large;
+  thumbnail: Large;
 }
 
 export type Story = {
-  id:         number;
+  id: number;
   attributes: {
-    type:          string;
-    title:         string;
-    content:       string;
+    type: string;
+    title: string;
+    content: string;
     released_date: Date | null;
-    character_id:  null;
-    slug:          string;
-    createdAt:     Date;
-    updatedAt:     Date;
-    locale:        Locale;
-    cover:         Cover;
+    character_id: null;
+    slug: string;
+    createdAt: Date;
+    updatedAt: Date;
+    locale: Locale;
+    cover: Cover;
     localizations: Localizations;
   };
-}
+};
 
-export type WeaponType = "SWORD" | "BIG_SWORD" | "SPEAR" | "FIST" | "STAFF" | "GUN";
+export type WeaponType =
+  | "SWORD"
+  | "BIG_SWORD"
+  | "SPEAR"
+  | "FIST"
+  | "STAFF"
+  | "GUN";
 
 export type ElementTypes = "DARK" | "LIGHT" | "FIRE" | "WATER" | "WIND";
 
 export type WeaponEvolutionStage = {
   AssetId: string;
   RarityType: CostumeRarity;
-}
+};
 
 export type WeaponSkillStage = {
   name: string;
   description: {
     long?: string;
     short?: string;
-  }
+  };
   DescriptionSkillTextId: number;
   LevelLowerLimit: number;
   NameSkillTextId: number;
   SkillAssetCategoryId: number;
   SkillAssetVariationId: number;
   SkillCooltimeValue: number;
-}
+};
 
 export type WeaponAbilityStage = {
   name: string;
   description: {
     long?: string;
     short?: string;
-  }
+  };
   DescriptionAbilityTextId: number;
   LevelLowerLimit: number;
   NameAbilityTextId: number;
   AbilityAssetCategoryId: number;
   AbilityAssetVariationId: number;
   AbilityCooltimeValue: number;
-}
+};
 
 export type Weapon = {
   ids: {
@@ -310,17 +316,17 @@ export type Weapon = {
     asset: string;
     catalogSort: number;
     catalogId: number;
-  },
+  };
   name: {
-    en: string,
-  },
+    en: string;
+  };
   skills: WeaponSkillStage[];
   abilities: WeaponAbilityStage[];
   evolutions: WeaponEvolutionStage[];
   stories: string[];
-  rarity: CostumeRarity,
-  type: WeaponType,
-  attribute: ElementTypes,
+  rarity: CostumeRarity;
+  type: WeaponType;
+  attribute: ElementTypes;
   isDark: boolean;
   isStory: boolean;
   isRestrictDiscard: boolean;
@@ -342,26 +348,25 @@ export type Weapon = {
       weaponId: string;
     };
     weapon: {
-      id: string,
-      inLibrary: boolean,
-      isDarkWeapon: boolean,
-      characterName: string,
-      characterTitle: string,
+      id: string;
+      inLibrary: boolean;
+      isDarkWeapon: boolean;
+      characterName: string;
+      characterTitle: string;
       sources: {
         difficulty: string;
         firstClear: boolean;
         groupName: string;
         questName: string;
         questType: string;
-        sourceType: string,
-        storeName: string
-      }[]
+        sourceType: string;
+        storeName: string;
+      }[];
     };
-  }
-}
+  };
+};
 
-export type CostumeRarity = "RARE" | "S_RARE" | "SS_RARE" | "LEGEND"
-
+export type CostumeRarity = "RARE" | "S_RARE" | "SS_RARE" | "LEGEND";
 
 export type CostumeStats = {
   agility: number;
@@ -370,7 +375,7 @@ export type CostumeStats = {
   cr: number;
   def: number;
   hp: number;
-}
+};
 
 export type CostumeStatsList = {
   base: {
@@ -384,8 +389,8 @@ export type CostumeStatsList = {
   maxWithAscension: {
     base: CostumeStats;
     displayed: CostumeStats;
-  }
-}
+  };
+};
 
 export type CostumeSource = {
   banner?: string;
@@ -396,7 +401,7 @@ export type CostumeSource = {
   questName?: string;
   questType?: string;
   storeName?: string;
-}
+};
 
 export type CostumeSkill = {
   SkillAssetCategoryId: number;
@@ -408,9 +413,9 @@ export type CostumeSkill = {
   description: {
     short: string;
     long: string;
-  }
+  };
   name: string;
-}
+};
 
 export type CostumeAbility = {
   AssetCategoryId: number;
@@ -422,105 +427,23 @@ export type CostumeAbility = {
   description: {
     short: string;
     long: string;
-  }
+  };
   name: string;
-}
+};
 
 export type CharacterRank = {
   amount: number;
   stat: string;
-  type: 'percent' | 'flat'
-}
-
-export type Costume = {
-  ids: {
-    costume: number,
-    character: number,
-    emblem: number,
-    actor: string,
-    material: number;
-  },
-  character: {
-    en: string,
-  },
-  costume: {
-    name: {
-      en: string,
-    },
-    description: {
-      en: string,
-    },
-    emblem: {
-
-    };
-    weaponType: WeaponType,
-    weapon: Weapon | null;
-    rarity: CostumeRarity,
-    stats: CostumeStatsList,
-  },
-  skills: CostumeSkill[][];
-  abilities: CostumeAbility[][];
-  metadata: {
-    name: string;
-    title: string;
-    inLibrary: boolean;
-    id: number;
-    rarity: number;
-    weapon: string;
-    weaponId: string | null;
-    weaponType: string;
-    story: string;
-    actions: {
-      skills: [];
-      abilities: [];
-    };
-    stats: {
-      absoluteMax: CostumeStats
-      maxNoAscension: CostumeStats
-      min: CostumeStats
-    };
-    sources: CostumeSource[];
-    ranks: {
-      name: string;
-      ranks: CharacterRank[];
-      totals: {
-        agility: {
-          flat: number;
-          percent: number;
-        }
-        atk: {
-          flat: number;
-          percent: number;
-        }
-        critDamage: {
-          flat: number;
-          percent: number;
-        }
-        critRate: {
-          flat: number;
-          percent: number;
-        }
-        def: {
-          flat: number;
-          percent: number;
-        }
-        hp: {
-          flat: number;
-          percent: number;
-        }
-      }
-    };
-    releaseDate: string | null;
-  },
-}
+  type: "percent" | "flat";
+};
 
 export interface NierNotification {
-  informationId: number
-  informationType: string
-  postscriptDatetime: number
-  publishStartDatetime: number
-  thumbnailImagePath: string | null
-  title: string
-  webviewMissionId: number
-  body?: string
+  informationId: number;
+  informationType: string;
+  postscriptDatetime: number;
+  publishStartDatetime: number;
+  thumbnailImagePath: string | null;
+  title: string;
+  webviewMissionId: number;
+  body?: string;
 }
