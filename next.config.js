9845b8cd471d2548f8a2516d7e506f21581c712f
Nemesis <contact@keziahmoselle.fr>
2023-02-16 18:39:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')({
 })
 
 module.exports = withPWA({
-  reactStrictMode: true,
+  reactStrictMode: false,
   experimental: {
     largePageDataBytes: 1028 * 1000,
   },
