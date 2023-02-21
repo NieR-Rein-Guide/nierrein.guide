f43b97d9d927540a7499b44da9b3f4197b94f007
Nemesis <contact@keziahmoselle.fr>
2023-02-21 19:43:00 +0100

    feat(api): fix strapi hook

--- src/pages/api/hooks/strapi.ts
+++ src/pages/api/hooks/strapi.ts
@@ -1,48 +1,56 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { Colors, EmbedBuilder } from 'discord.js';
+import { Colors, EmbedBuilder } from "discord.js";
 import { env } from "../../../env";
 import { discordWebhook } from "@libs/webhook";
 import { format } from "date-fns";
 
 const STRAPI_EVENT_TYPES = {
-  'record': {
-    color: Colors.Blurple
+  record: {
+    color: Colors.Blurple,
   },
-  'premium_summons': {
-    color: Colors.Purple
+  fate_board: {
+    color: Colors.Blue,
   },
-  'weekly_summons': {
-    color: Colors.DarkPurple
+  premium_summons: {
+    color: Colors.Purple,
   },
-  'monthly_summons': {
-    color: Colors.DarkPurple
+  weekly_summons: {
+    color: Colors.DarkPurple,
   },
-  'abyss_tower': {
-    color: Colors.DarkBlue
+  monthly_summons: {
+    color: Colors.DarkPurple,
   },
-  'variation': {
-    color: Colors.Green
+  abyss_tower: {
+    color: Colors.DarkBlue,
   },
-  'anecdote': {
-    color: Colors.Yellow
+  variation: {
+    color: Colors.Green,
   },
-  'campaign': {
-    color: Colors.White
+  anecdote: {
+    color: Colors.Yellow,
   },
-}
+  campaign: {
+    color: Colors.White,
+  },
+};
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
     try {
-      const event = req.body
-      const entry = event.entry
+      const event = req.body;
+      const entry = event.entry;
 
       if (!env.DISCORD_EVENTS_WEBHOOK_URL) {
-        return console.warn('Missing optional env var: DISCORD_EVENTS_WEBHOOK_URL');
+        return console.warn(
+          "Missing optional env var: DISCORD_EVENTS_WEBHOOK_URL"
+        );
       }
 
-      if (!event.model === 'event') {
-        return console.warn('STRAPI: unsupported event type', event)
+      if (!event.model === "event") {
+        return console.warn("STRAPI: unsupported event type", event);
       }
 
       const embed = new EmbedBuilder()
@@ -50,27 +58,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         .setURL(`https://nierrein.guide/event/${entry.slug}`)
         .setImage(entry.image.url)
         .setTimestamp(new Date(entry.updatedAt))
-        .setColor(STRAPI_EVENT_TYPES[entry.type].color ?? Colors.Blurple)
+        .setColor(STRAPI_EVENT_TYPES[entry.type].color ?? Colors.Blurple);
 
-      const startDate = format(new Date(entry.start_date), 'd MMM yyyy')
-      const endDate = format(new Date(entry.end_date), 'd MMM yyyy')
-      let description = `Event period: ${startDate} - ${endDate}`
+      const startDate = format(new Date(entry.start_date), "d MMM yyyy");
+      const endDate = format(new Date(entry.end_date), "d MMM yyyy");
+      let description = `Event period: ${startDate} - ${endDate}`;
 
       if (entry.gems > 0) {
-        description += `\nTotal gems obtainable: <:gem:1069908865891704842> ${entry.gems}`
+        description += `\nTotal gems obtainable: <:gem:1069908865891704842> ${entry.gems}`;
       }
 
-      embed.setDescription(description)
+      embed.setDescription(description);
 
       await discordWebhook.send({
-        embeds: [embed]
-      })
+        embeds: [embed],
+      });
 
-      return res.status(200).json(entry)
+      return res.status(200).json(entry);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   } else {
-    return res.status(405).json({ message: 'Method Not Allowed' })
+    return res.status(405).json({ message: "Method Not Allowed" });
   }
 }
