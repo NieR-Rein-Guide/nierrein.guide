e0ef2b10babf400598b0cfa0dcc8d04f4b514cb2
Nemesis <contact@keziahmoselle.fr>
2023-03-13 16:06:47 +0100

    feat: allow preview guide url

--- src/models/guide.ts
+++ src/models/guide.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { env } from "../env";
-import { Guide } from "./types"
+import { Guide } from "./types";
 
 async function getAllGuides(): Promise<Guide[]> {
   const { data } = await axios.get(
-    `${env.NEXT_PUBLIC_STRAPI_REST_API_ENDPOINT}/guides?sort[0]=updatedAt:desc&populate=*`,);
+    `${env.NEXT_PUBLIC_STRAPI_REST_API_ENDPOINT}/guides?sort[0]=updatedAt:desc&populate=*`
+  );
 
   const guides: Guide[] = data.data;
 
@@ -13,7 +14,7 @@ async function getAllGuides(): Promise<Guide[]> {
 
 async function getGuideBySlug(slug: string): Promise<Guide> {
   const { data } = await axios.get(
-    `${env.NEXT_PUBLIC_STRAPI_REST_API_ENDPOINT}/guides?filters[slug][$eqi]=${slug}&populate=*`
+    `${env.NEXT_PUBLIC_STRAPI_REST_API_ENDPOINT}/guides?filters[slug][$eqi]=${slug}&populate=*&publicationState=preview`
   );
 
   return data.data?.[0];
@@ -21,13 +22,10 @@ async function getGuideBySlug(slug: string): Promise<Guide> {
 
 async function getGuideById(id: number): Promise<Guide> {
   const { data } = await axios.get(
-    `${env.NEXT_PUBLIC_STRAPI_REST_API_ENDPOINT}/guides/${id}?populate=*`);
+    `${env.NEXT_PUBLIC_STRAPI_REST_API_ENDPOINT}/guides/${id}?populate=*`
+  );
 
   return data.data;
 }
 
-export {
-  getAllGuides,
-  getGuideBySlug,
-  getGuideById,
-}
+export { getAllGuides, getGuideBySlug, getGuideById };
