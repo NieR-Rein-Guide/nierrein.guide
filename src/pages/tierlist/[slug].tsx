b497a488dc6ccfd2c56d8f82fed2d075fbeee36c
Nemesis <contact@keziahmoselle.fr>
2023-05-31 17:27:30 +0200

    fix: wysiwyg content in tooltip and tierlist notes inline

--- src/pages/tierlist/[slug].tsx
+++ src/pages/tierlist/[slug].tsx
@@ -487,7 +487,7 @@ export function TierlistContent({ tierlist, items }: TierListProps) {
 
                         {showNotesInline && (
                           <div
-                            className="flex-1 ml-4 max-w-xl"
+                            className="wysiwyg flex-1 ml-4 max-w-xl"
                             dangerouslySetInnerHTML={{
                               __html: tierItem.tooltip,
                             }}
