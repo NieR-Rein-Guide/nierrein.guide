bed700490fe0769840d8731e9388b6a00718f40d
KeziahMoselle <contact@keziahmoselle.fr>
2024-03-13 13:13:22 +0100

    remove unecessary env

--- src/env.ts
+++ src/env.ts
@@ -7,21 +7,6 @@ export const env = envsafe({
   }),
 
   /**
-   * API
-   */
-  NEXT_PUBLIC_API_ENDPOINT: url({
-    default: "https://strapi.nierrein.guide/",
-  }),
-  NEXT_PUBLIC_STRAPI_REST_API_ENDPOINT: url({}),
-
-  /**
-   * Search
-   */
-  NEXT_PUBLIC_SEARCH_ENDPOINT: url({
-    default: "https://search.nierrein.guide",
-  }),
-
-  /**
    * Databases
    */
   DATABASE_URL: str({
@@ -30,10 +15,4 @@ export const env = envsafe({
   NIERREINGUIDE_DATABASE_URL: str({
     desc: "Main database (loadouts...)",
   }),
-
-  /**
-   * Post new events on Discord
-   */
-  DISCORD_EVENTS_WEBHOOK_URL: url({}),
-  DISCORD_TIERLISTS_UPDATE_CHANNEL_WEBHOOK: url({}),
 });
